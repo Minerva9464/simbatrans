2b0fe69c167804849c1428ded7c6e626ff33ac3b
MHosein94 <mh.taghavi94@gmail.com>
2023-11-15 20:01:35 -0500

    Learning Rate Modified, Get Much Better R2-Score Needed folders added to git (Modified .gitignore)

---
+++ Models/Readme.txt
@@ -0,0 +1,1 @@
+This folder must contains all learned models
--- Transformer/Train.py
+++ Transformer/Train.py
@@ -1,6 +1,7 @@
 from Transformer import Transformer
 import torch
 import torch.nn as nn
+from torchmetrics.functional import r2_score, mean_absolute_percentage_error
 import pandas as pd
 from tqdm import tqdm
 import torch.optim as optim
@@ -8,12 +9,18 @@ import numpy as np
 
 def train_model(crypto_name, d_model, h, N, d_FF, seqlen_encoder, 
                 seqlen_decoder, f, p, epoch_number, batch_size, 
-                learning_rate, loss_function
+                loss_function
                 ):
-    device=torch.device('cuda:0')
+    
+    if torch.cuda.is_available():
+        torch.cuda.empty_cache()
+        device=torch.device('cuda:0')
+    else:
+        device=torch.device('cpu')
+        
     torch.set_default_device(device)
     
-    crypto_prices=pd.read_csv(f'Processed Prices/{crypto_name} Train.csv').astype('float32').iloc[:10000, :]
+    crypto_prices=pd.read_csv(f'Processed Prices/{crypto_name} Train.csv').astype('float32').iloc[:, :]
 
     crypto_prices=torch.tensor(np.array(crypto_prices))
 
@@ -27,26 +34,29 @@ def train_model(crypto_name, d_model, h, N, d_FF, seqlen_encoder,
         params=transformer.parameters(),
         betas=(0.9,0.98),
         eps=1e-9,
-        lr=learning_rate
+        lr=0
     )
 
     transformer.train()
-
+    # print(transformer.parameters().)
     for epoch in range(epoch_number):
+        step_num = epoch+1
         for row in tqdm(range(0, len_crypto_prices, batch_size)):
-            optimizer.zero_grad()
+            optimizer.param_groups[0]['lr'] = d_model**(-0.5)*min(step_num**(-0.5), step_num*4000**(-1.5))
+            optimizer.zero_grad() # x-grad=0
 
             inputs=crypto_prices[row: row+batch_size, 0:seqlen_encoder]
             outputs=crypto_prices[row: row+batch_size, seqlen_encoder-1: -1]
             targets=crypto_prices[row: row+batch_size, seqlen_encoder:] # chizi hast ke bayad behesh beresim
 
             predicted_outputs=transformer.forward(inputs, outputs).squeeze()
-            loss=loss_function.forward(predicted_outputs, targets)
+            
+            loss=loss_function(predicted_outputs, targets) if loss_function != r2_score else 1-loss_function(predicted_outputs, targets)
             loss.backward() # x.grad += dloss/dx
-
             optimizer.step() # x += -lr*x.grad
-            # print(loss.item())
-        # print(f'Epoch: {epoch+1}, Loss: {loss.item()}')
+            
+        r2_score_computed = r2_score(predicted_outputs, targets)
+        print(f'Epoch: {step_num}, Loss: {loss.item()}, R2 Score: {r2_score_computed}')
 
     torch.save(transformer, f'Models/{crypto_name} Model.pth')
         
@@ -62,19 +72,21 @@ if __name__=='__main__':
     p=0.1
     
     # Learning Parameters
-    epoch_number=2
-    batch_size=50
-    learning_rate=1e-3
-    loss_function=nn.L1Loss()
+    epoch_number=100
+    batch_size=100
+    loss_function=nn.MSELoss() # MSE
+    # loss_function=nn.L1Loss() # MAE
+    # loss_function=mean_absolute_percentage_error # MAPE
+    # loss_function=r2_score # R2-Score
 
     train_model('Bitcoin', d_model, h, N, d_FF, seqlen_encoder, 
                 seqlen_decoder, f, p, epoch_number, batch_size, 
-                learning_rate, loss_function
+                loss_function
                 )
 
     train_model('Ethereum', d_model, h, N, d_FF, seqlen_encoder, 
                 seqlen_decoder, f, p, epoch_number, batch_size, 
-                learning_rate, loss_function
+                loss_function
                 )
 
 
